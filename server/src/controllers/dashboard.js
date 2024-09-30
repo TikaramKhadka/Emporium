2ac2a84c39aeae8e55f016ee39ea2c2b8acc8bcb
Tikaramkhadka <tikaram.itexperts@gmail.com>
2024-09-30 22:34:26 +0545

    commit dashboard

--- server/src/controllers/dashboard.js
+++ server/src/controllers/dashboard.js
@@ -1,49 +1,30 @@
-const Category = require('../models/category')
-const Product = require('../models/product')
-const User = require('../models/user')
-const Brand = require('../models/brand')
+// src/controllers/dashboard.js
+const Category = require('../models/category');
+const Product = require('../models/product');
+const User = require('../models/user');
+const Brand = require('../models/brand');
 
+// Get counts for Users, Products, Categories, and Brands
+const getCounts = async (req, res) => {
+    try {
+        const userCount = await User.countDocuments();
+        const productCount = await Product.countDocuments();
+        const categoryCount = await Category.countDocuments();
+        const brandCount = await Brand.countDocuments();
+        const salesCount = 0;
+        const ordersCount = 0;
 
-
-// get user count
-const getUserCount = async (req, res) => {
-    try {
-        const data = await User.countDocuments();
-        console.log(data)
-        res.status(200).send({count:data});
+        res.json({
+            users: { title: 'Users', count: userCount },
+            products: { title: 'Products', count: productCount },
+            categories: { title: 'Categories', count: categoryCount },
+            brands: { title: 'Brands', count: brandCount },
+            sales: { title: 'Sales', count: salesCount },
+            orders: { title: 'Orders', count: ordersCount }
+        });
     } catch (error) {
-        res.status(404).send({ msg: "Data not found", error });
-    }
-};
-// get product count
-const getProductCount = async (req, res) => {
-    try {
-        const data = await Product.countDocuments();
-        res.status(200).send(data);
-    } catch (error) {
-        res.status(404).send({ msg: "Data not found", error });
-    }
-};
-// get category count
-const getCategoryCount = async (req, res) => {
-    try {
-        const data = await Category.countDocuments();
-        res.status(200).send(data);
-    } catch (error) {
-        res.status(404).send({ msg: "Data not found", error });
+        res.status(500).json({ msg: 'Error fetching counts', error });
     }
 };
 
-// get brand count
-const getBrandCount = async (req, res) => {
-    try {
-        const data = await Brand.countDocuments();
-        res.sendStatus(200).send(data);
-    } catch (error) {
-        res.sendStatus(404).send({ msg: "Data not found", error });
-    }
-};
-
-
-module.exports ={getUserCount,getProductCount, getCategoryCount, getBrandCount}
-
+module.exports = { getCounts };
--- server/src/route/dashboard.js
+++ server/src/route/dashboard.js
@@ -1,16 +1,8 @@
 const Router = require('express');
-const { getUserCount, getProductCount, getCategoryCount, getBrandCount}=require('../controllers/dashboard')
-    const DashboardRoute = Router();
-  
-    // get product count
-    DashboardRoute.get('/productcounts', getProductCount);
-    // get user count
-    DashboardRoute.get('/usercounts', getUserCount);
-    // get category count
-    DashboardRoute.get('/categorycounts', getCategoryCount);
-    // get brand
-    DashboardRoute.post('/brandcounts',getBrandCount);
-    // //get order count
-    // DashboardRoute.put('/ordercount', getOrderCount) 
+const { getCounts } = require('../controllers/dashboard');
+const DashboardRoute = Router();
 
-    module.exports = DashboardRoute;
+// Route to get all counts
+DashboardRoute.get('/counts', getCounts);
+
+module.exports = DashboardRoute;
